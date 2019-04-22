b3463b0a7c3b3481d5fbe77bc33798c0d9bd393e
Murto <murto@airmail.cc>
2019-04-22 17:57:09 +0100

    Add string conversion to enum class

--- include/TokenKind.hpp
+++ include/TokenKind.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdexcept>
+
 
 enum class TokenKind {
   STATE,
@@ -12,3 +14,18 @@ enum class TokenKind {
   CHARACTER,
   EOI
 };
+
+std::string to_string(TokenKind kind) {
+  switch (kind) {
+    case TokenKind::STATE: return "STATE";
+    case TokenKind::INITIAL: return "INITIAL";
+    case TokenKind::ACCEPTING: return "ACCEPTING";
+    case TokenKind::TRANSITION: return "TRANSITION";
+    case TokenKind::RIGHTARROW: return "RIGHTARROW";
+    case TokenKind::EPSILON: return "EPSILON";
+    case TokenKind::STRING: return "STRING";
+    case TokenKind::CHARACTER: return "CHARACTER";
+    case TokenKind::EOI: return "EOI";
+  }
+  throw std::runtime_error{"Unknown TokenKind"};
+}
