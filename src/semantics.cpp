d2b6b9d9ee8a2fdb0bd74d39d14a14b73333bfde
Murray Steele <contact@murto.dev>
2019-05-17 13:44:32 +0100

    Replace code with call to function

--- src/semantics.cpp
+++ src/semantics.cpp
@@ -1,4 +1,5 @@
 #include "ProgramParseTreeNode.hpp"
+#include "semantics.hpp"
 #include "StateDeclParseTreeNode.hpp"
 #include "TransitionDeclParseTreeNode.hpp"
 
@@ -9,16 +10,7 @@
 
 void assert_semantics(const ProgramParseTreeNode& program) {
   std::unordered_set<std::string> state_names;
-  bool initial = false;
-  for (auto it = program.states_begin(); it != program.states_end(); ++it) {
-    state_names.insert(it->name());
-    if (it->initial()) {
-      initial = true;
-    }
-  }
-  if (!initial) {
-    throw std::runtime_error{"Semantic error"};
-  }
+  assert_initial_state_existance(program);
   for (auto it = program.transitions_begin(); it != program.transitions_end(); ++it) {
     if (state_names.find(it->from()) == state_names.end()) {
       throw std::runtime_error{"Semantic error"};
