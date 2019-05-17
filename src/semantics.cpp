e948fff023d3f23532a0bf7b4d2d5e585297df16
Murray Steele <contact@murto.dev>
2019-05-17 13:40:53 +0100

    Implement initial state semantics function

--- src/semantics.cpp
+++ src/semantics.cpp
@@ -40,6 +40,15 @@ void assert_discrete_state_names(const ProgramParseTreeNode& program) {
   }
 }
 
+void assert_initial_state_existance(const ProgramParseTreeNode& program) {
+  for (auto it = program.states_begin(); it != program.states_end(); ++it) {
+    if (it->initial()) {
+      return
+    } else {
+      throw std::runtime_error{"Semantic error"};
+    }
+}
+
 void assert_transition_states_existance(const ProgramParseTreeNode& program) {
   std::unordered_set<std::string> state_names;
   for (auto it = program.states_begin(); it != program.states_end(); ++it) {
