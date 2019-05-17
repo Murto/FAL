e787736d52de558220f0acdc288646d0437c7987
Murray Steele <contact@murto.dev>
2019-05-17 13:35:22 +0100

    Implement semantic functions

--- src/semantics.cpp
+++ src/semantics.cpp
@@ -28,3 +28,29 @@ void assert_semantics(const ProgramParseTreeNode& program) {
     }
   }
 }
+
+void assert_discrete_state_names(const ProgramParseTreeNode& program) {
+  std::unordered_set<std::string> state_names;
+  for (auto it = program.states_begin(); it != program.states_end(); ++it) {
+    if (state_names.find(it->name()) == state_names.end()) {
+      state_names.insert(it->name());
+    } else {
+      throw std::runtime_error{"Semantic error"};
+    }
+  }
+}
+
+void assert_transition_states_existance(const ProgramParseTreeNode& program) {
+  std::unordered_set<std::string> state_names;
+  for (auto it = program.states_begin(); it != program.states_end(); ++it) {
+    state_names.insert(it->name());
+  }
+  for (auto it = program.transitions_begin(); it != program.transitions_begin(); ++it) {
+    if (state_names.find(it->from()) == state_names.end()) {
+      throw std::runtime_error{"Semantic error"};
+    }
+    if (state_names.find(it->to()) == state_names.end()) {
+      throw std::runtime_error{"Semantic error"};
+    }
+  }
+}
