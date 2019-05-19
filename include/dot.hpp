484964de971eff6753411bafe808a2c9473dc3e1
Murray Steele <contact@murto.dev>
2019-05-19 12:28:40 +0100

    Remove old code

--- include/dot.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "ProgramParseTreeNode.hpp"
-
-#include <string>
-
-
-void generate_dot(const ProgramParseTreeNode& program, const std::string& file_path);
--- include/image.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "ProgramParseTreeNode.hpp"
-
-#include <string>
-
-
-void generate_png(const ProgramParseTreeNode& program, const std::string& file_path);
