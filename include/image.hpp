3c3e9837f50299d3c97b13fa55f367c0ef5f03e0
Murray Steele <contact@murto.dev>
2019-05-19 12:16:15 +0100

    Remove unneeded includes

--- include/image.hpp
+++ include/image.hpp
@@ -2,11 +2,7 @@
 
 #include "ProgramParseTreeNode.hpp"
 
-#include <graphviz/cgraph.h>
-#include <graphviz/gvc.h>
-
 #include <string>
-#include <unordered_map>
 
 
 void generate_png(const ProgramParseTreeNode& program, const std::string& file_path);
