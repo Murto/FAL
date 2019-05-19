df831c6459e69f0a86ff8c60721754b41088c13e
Murray Steele <contact@murto.dev>
2019-05-19 13:16:40 +0100

    Add postscript format

--- src/fal.cpp
+++ src/fal.cpp
@@ -47,6 +47,8 @@ int main(int argc, char** argv) {
         generate_dot(program, input_file_path + ".dot");
       } else if (vm["format"].as<std::string>() == "png") {
         generate_png(program, input_file_path + ".png");
+      } else if (vm["format"].as<std::string>() == "ps") {
+        generate_postscript(program, input_file_path + ".ps");
       } else {
         std::cerr << "Invalid output format";
       }
